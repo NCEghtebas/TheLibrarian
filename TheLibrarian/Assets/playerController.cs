1476e56391cf5ebb54865462b295a58d89a2f527
Chloe Eghtebas <nceghtebas@hotmail.com>
2015-10-24 19:53:49 -0400

    adding preliminary character movement wasd keys

---
+++ TheLibrarian/Assets/playerController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+	private Vector3 moveDirection = Vector3.zero;
+	void Update() {
+		CharacterController controller = GetComponent<CharacterController>();
+		moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		Debug.Log (Input.GetAxis("Horizontal"));
+		moveDirection = transform.TransformDirection(moveDirection);
+		moveDirection *= speed;
+		controller.Move(moveDirection * Time.deltaTime);
+//		if (OVRManager.isHSWDisplayed)
+//		{
+//			if (Input.anyKeyDown)
+//			{
+//				OVRManager.DismissHSWDisplay();
+//			}
+//		}
+	}
+}
