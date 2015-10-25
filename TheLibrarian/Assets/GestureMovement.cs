947f3505ecc2e71cadddb39a563998f125324af2
Chloe Eghtebas <nceghtebas@hotmail.com>
2015-10-24 21:47:29 -0400

    startign gesture controlled movement

---
+++ TheLibrarian/Assets/GestureMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class GestureMovement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Controller controller = GetComponent<Controller>();
+		controller.EnableGesture (Gesture.GestureType.TYPE_KEY_TAP);
+		Debug.Log ("Gestate");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
